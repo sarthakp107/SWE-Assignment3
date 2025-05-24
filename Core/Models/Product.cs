fe6fa915c79938def5cf8cabc0fa8996122c4241
prod-chirag <chiragluitel32@gmail.com>
2025-05-24 22:33:12 +1000

    Added Product class

---
+++ Core/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿namespace AWE_Electronics.Core.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int Stock { get; set; }
+
+        //Constructor
+        public Product(string name, string description, int price, int stock)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            Stock = stock;
+        }
+
+        //Methods
+        public override string ToString() //Overriding the ToString function to display Product Info
+        {
+            return $"{Name} - {Description} - ${Price}";
+        }
+
+    }
+}
